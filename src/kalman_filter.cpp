0bdef426d5b537d420c9eb32154d82f9c4119281
root <root@w10.localdomain>
2018-01-25 11:42:06 -0800

    for submission

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -1,9 +1,11 @@
 #include "kalman_filter.h"
 
+#include <iostream>
+
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
-// Please note that the Eigen library does not initialize 
+// Please note that the Eigen library does not initialize
 // VectorXd or MatrixXd objects with zeros upon creation.
 
 KalmanFilter::KalmanFilter() {}
@@ -11,32 +13,86 @@ KalmanFilter::KalmanFilter() {}
 KalmanFilter::~KalmanFilter() {}
 
 void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
-                        MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
-  x_ = x_in;
-  P_ = P_in;
-  F_ = F_in;
-  H_ = H_in;
-  R_ = R_in;
-  Q_ = Q_in;
+						MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in)
+{
+	x_ = x_in;
+	P_ = P_in;
+	F_ = F_in;
+	H_ = H_in;
+	R_ = R_in;
+	Q_ = Q_in;
 }
 
-void KalmanFilter::Predict() {
-  /**
-  TODO:
+void KalmanFilter::Predict()
+{
+   /**
     * predict the state
-  */
+    */
+	x_ = F_ * x_;
+	MatrixXd Ft = F_.transpose();
+	P_ = F_ * P_ * Ft + Q_;
 }
 
-void KalmanFilter::Update(const VectorXd &z) {
-  /**
-  TODO:
+// normalize angle to range [-pi, pi]
+inline float normalize_range(float phi) // TODO: put this in one place
+{
+	while (phi > M_PI)
+		phi -= 2*M_PI;
+	while (phi < -M_PI)
+		phi += 2*M_PI;
+	return phi;
+}
+
+void KalmanFilter::Update(const VectorXd &z)
+{
+   /**
     * update the state by using Kalman Filter equations
-  */
+    */
+	VectorXd z_pred = H_ * x_;
+	VectorXd y = z - z_pred;
+	MatrixXd Ht = H_.transpose();
+	MatrixXd S = H_ * P_ * Ht + R_;
+	MatrixXd Sinv = S.inverse();
+	MatrixXd PHt = P_ * Ht;
+	MatrixXd K = PHt * Sinv;
+
+	long x_size = x_.size();
+	MatrixXd I = MatrixXd::Identity(x_size, x_size); // TODO: cache this constant
+
+	//new estimate
+	x_ = x_ + (K * y);
+	P_ = (I - K * H_) * P_;
 }
 
-void KalmanFilter::UpdateEKF(const VectorXd &z) {
-  /**
-  TODO:
+void KalmanFilter::UpdateEKF(const VectorXd &z)
+{
+   /**
     * update the state by using Extended Kalman Filter equations
-  */
+    */
+	float px = x_(0);
+	float py = x_(1);
+	float vx = x_(2);
+	float vy = x_(3);
+	float rho = sqrt(px*px + py*py);
+	float phi = atan2(py, px); 
+	float rho_dot = rho < 0.00001 ? 0.0 : (px*vx + py*vy)/rho;
+
+	VectorXd z_pred = VectorXd(3);
+	z_pred << rho, phi, rho_dot;
+
+	VectorXd y = z - z_pred;
+	y(1) = normalize_range(y(1));
+
+	MatrixXd Ht = H_.transpose();
+	MatrixXd S = H_ * P_ * Ht + R_;
+	MatrixXd Sinv = S.inverse(); 
+	MatrixXd PHt = P_ * Ht;
+	MatrixXd K = PHt * Sinv;
+
+	long x_size = x_.size();
+	MatrixXd I = MatrixXd::Identity(x_size, x_size); // TODO: cache this constant
+
+	//new estimate
+	x_ = x_ + (K * y);
+	P_ = (I - K * H_) * P_;
 }
--- src/tools.cpp
+++ src/tools.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include "tools.h"
 
-using Eigen::VectorXd;
 using Eigen::MatrixXd;
+using Eigen::VectorXd;
 using std::vector;
 
 Tools::Tools() {}
@@ -10,16 +10,65 @@ Tools::Tools() {}
 Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
-                              const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+							  const vector<VectorXd> &ground_truth)
+{
+	VectorXd rmse(4);
+	rmse << 1000, 1000, 1000, 1000; // init to "bad" value
+
+	// check the validity of the following inputs:
+	//  * the estimation vector size should not be zero
+	//  * the estimation vector size should equal ground truth vector size
+	if (estimations.size() == 0)
+		cout << "CalculateRMSE(): empty estimations vector" << endl;
+	else if (estimations.size() != ground_truth.size())
+		cout << "CalculateRMSE(): inconsistent vector size" << endl;
+	else
+	{
+
+		// accumulate squared residuals
+		rmse << 0, 0, 0, 0;
+		for (int i = 0; i < estimations.size(); ++i) {
+			VectorXd diff = estimations[i] - ground_truth[i];
+			VectorXd diff_sq = diff.array() * diff.array();
+			rmse += diff_sq;
+		}
+
+		// calculate the mean
+		rmse *= 1.0 / estimations.size();
+
+		// calculate the square root
+		rmse = rmse.array().sqrt();
+	}
+
+	//return the result
+	return rmse;
 }
 
-MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-  /**
-  TODO:
-    * Calculate a Jacobian here.
-  */
+MatrixXd Tools::CalculateJacobian(const VectorXd &x_state)
+{
+
+	const float TOO_SMALL = 0.0001; // TODO: maybe better choice here
+
+	MatrixXd Hj(3, 4);
+	//recover state parameters
+	float px = x_state(0);
+	float py = x_state(1);
+	float vx = x_state(2);
+	float vy = x_state(3);
+
+	//check division by zero
+	float rho_mag_sq = px*px + py*py;
+	if (rho_mag_sq < TOO_SMALL) {
+		cout << "CalculateJacobian(): ill-conditioned matrix" << endl;
+		return Hj; // TODO: maybe better choice here
+	}
+
+	//compute the Jacobian matrix
+	float rho_mag = std::sqrt(rho_mag_sq);
+	float rho_mag_3by2 = rho_mag_sq * rho_mag / 2;
+	Hj << 	px/rho_mag, py/rho_mag, 0.0, 0.0,
+			-py/rho_mag_sq, px/rho_mag_sq, 0.0, 0.0,
+			py*(vx*py - vy*px)/rho_mag_3by2, px*(vy*px - vx*py)/rho_mag_3by2, px/rho_mag, py/rho_mag;
+
+	return Hj;
 }
